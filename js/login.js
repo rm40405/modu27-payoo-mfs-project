04715431cfdbdd5ad990f69685823b665e65da3d
rm40405 <rm404085@gmail.com>
2024-09-20 08:35:24 +0600

    add the another html page

---
+++ js/login.js
@@ -0,0 +1,24 @@
+// console.log("button file addd");
+// search: from submit reloading the page
+document.getElementById('login-button').addEventListener('click', function(event){
+    event.preventDefault();  //bejal lagbe 
+    console.log('clicked');
+    // submit pfone number
+    const mobileNumber = document.getElementById('mobile-number').value;
+     console.log(mobileNumber);
+    //  submit here pin number
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(pinNumber);
+    // this temporary you should not like this;
+    if(mobileNumber ==='5' && pinNumber ==='1234'){
+        console.log('you are logged in');
+        // window location href home page;
+        window.location.href = './homePage.html';
+    }
+    else{
+        alert('wrong number logged in');
+    }
+
+
+});
+

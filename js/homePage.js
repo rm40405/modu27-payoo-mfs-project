b6e043b2b72949205e717049583621c303b4fc70
rm40405 <rm404085@gmail.com>
2024-09-20 11:28:58 +0600

    add the add money function

---
+++ js/homePage.js
@@ -0,0 +1,27 @@
+// get the event handler for 
+// step 1:
+
+document.getElementById('add-money-btn').addEventListener('click',function(event){
+    // prevent form submission
+    // step 2:
+    event.preventDefault();
+    // get the amount
+    const addAmount = document.getElementById('add-amount').value;
+    const addNewAmount = parseFloat(addAmount);
+    
+    // get the pin number
+    const addPin = document.getElementById('add-pin').value;
+    
+    if(addPin=== '1234'){
+        // add the amount to the balance
+        const balance = document.getElementById('balance').innerText;
+        const newBalance = parseFloat(balance);
+        const updatedBalance = newBalance + addNewAmount;
+        document.getElementById('balance').innerText = updatedBalance;
+        
+    }
+    else{
+        alert('wrong pin or amount');
+    }
+
+});
